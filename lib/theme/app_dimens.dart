0ae2ed4fc3b639ebc4830010690eecfe34474744
Aman-Soule <amansoule9@gmail.com>
2026-08-24 12:44:36 +0000

    style: ajout des constantes de style (espacements, rayons, durees)

---
+++ lib/theme/app_dimens.dart
@@ -0,0 +1,30 @@
+
+class AppDimens {
+  AppDimens._();
+
+  // Espacements -------------------------------------------------------
+
+  static const double espaceXS = 4;
+  static const double espaceS = 8;
+  static const double espaceM = 12;
+  static const double espaceL = 16;
+  static const double espaceXL = 24;
+  static const double espaceXXL = 32;
+
+  // Rayons de bordure ---------------------------------------------------
+
+  static const double rayonPetit = 12;
+  static const double rayonMoyen = 16;
+  static const double rayonGrand = 20;
+  static const double rayonCarte = 24;
+
+  // Durées d'animation ---------------------------------------------------
+
+  static const Duration animationRapide = Duration(milliseconds: 200);
+  static const Duration animationStandard = Duration(milliseconds: 350);
+  static const Duration animationLente = Duration(milliseconds: 600);
+
+  // Courbes ---------------------------------------------------------------
+
+  static const double epaisseurJauge = 16;
+}
